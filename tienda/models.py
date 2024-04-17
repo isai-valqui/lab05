e49e27207d6fa9161f87e0795bdcd03d3b7efd29
isai-valqui <isai.valqui@tecsup.edu.pe>
2024-04-16 20:37:56 -0500

    first commit

---
+++ tienda/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+class Categoria (models.Model):
+    nombre = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self): 
+        return self.nombre
+    
+class Producto(models.Model):
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=200)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    stock = models.IntegerField(default=0)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self): 
+        return self.nombre
+
+class Cliente(models.Model):
+    nombres = models.CharField(max_length=200)
+    apellidos = models.CharField(max_length=200)
+    dni = models.CharField(max_length=8, unique=True)
+    telefono = models.CharField(max_length=20)
+    direccion = models.CharField(max_length=200)
+    email = models.EmailField()
+    fecha_nacimiento = models.DateField()
+    fecha_publicacion = models.DateTimeField('fecha publicada')
+
+    def __str__(self):
+        return f"{self.nombres} {self.apellidos}"
+
+   
